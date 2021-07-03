cc12baa0f048eb99fcac655ecbe90d401d0b0166
Rachel <rachelsuk97@gmail.com>
2021-07-03 23:12:52 +0000

    filled in change_fill_char method for shape class and separated create_shape fxnality into its own method

--- takehome.py
+++ takehome.py
@@ -23,6 +23,9 @@ class Shape():
         self.end_y = end_y
         self.height = (end_y-start_y) + 1
         self.fill_char = fill_char
+        self.create_shape()
+
+    def create_shape(self):
         self.shape = []
         row_num = self.end_y
         for num in range(self.end_y+1):
@@ -30,17 +33,21 @@ class Shape():
                 row_str = " " * self.width
             else:
                 row_str = (" " * (self.start_x)) + \
-                    (fill_char * (self.width))
+                    (self.fill_char * (self.width))
             self.shape.append(row_str)
             row_num -= 1
 
     def change_fill_char(self, char):
-        pass
+        self.fill_char = char
+        self.create_shape()
 
     def translate(self, axis, num):
         pass
 
 
 s = Shape(1, 2, 3, 4, 'a')
+for row in s.shape:
+    print(row)
+s.change_fill_char('b')
 for row in s.shape:
     print(row)
