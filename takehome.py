d2acd1b86ddfee4c4ae0709bb35e2bd6d2b24aea
Rachel <rachelsuk97@gmail.com>
2021-07-03 23:24:08 +0000

    filled in translate method for shape class

--- takehome.py
+++ takehome.py
@@ -21,33 +21,46 @@ class Shape():
         self.width = (end_x-start_x) + 1
         self.start_y = start_y
         self.end_y = end_y
-        self.height = (end_y-start_y) + 1
+        self.height = (start_y-end_y) + 1
         self.fill_char = fill_char
         self.create_shape()
 
     def create_shape(self):
         self.shape = []
-        row_num = self.end_y
-        for num in range(self.end_y+1):
-            if row_num < (self.start_y):
+        for num in range(self.start_y+1):
+            if num < (self.end_y):
                 row_str = " " * self.width
             else:
                 row_str = (" " * (self.start_x)) + \
                     (self.fill_char * (self.width))
             self.shape.append(row_str)
-            row_num -= 1
 
     def change_fill_char(self, char):
         self.fill_char = char
         self.create_shape()
 
     def translate(self, axis, num):
-        pass
+        if axis == "x":
+            if (self.start_x + num) < 0:
+                print("x coordinate cannot be a negative number")
+            else:
+                self.start_x += num
+                self.end_x += num
+        elif axis == "y":
+            if (self.end_y + num) < 0:
+                print("y coordinate cannot be a negative number")
+            else:
+                self.start_y += num
+                self.end_y += num
+        self.create_shape()
 
 
-s = Shape(1, 2, 3, 4, 'a')
+s = Shape(1, 4, 3, 2, 'a')
 for row in s.shape:
     print(row)
 s.change_fill_char('b')
+for row in s.shape:
+    print(row)
+s.translate('x', -5)
 for row in s.shape:
     print(row)
