4ee683adaa1b8d1e9470f77109867c4b0c1ad852
Rachel <rachelsuk97@gmail.com>
2021-07-07 03:50:31 +0000

    fixed add_shape functionality

--- takehome.py
+++ takehome.py
@@ -2,13 +2,9 @@ class Canvas():
     def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.empty_canvas_matrix = []
         self.shapes = []
         self.canvas_matrix = []
-        for num in range(self.height):
-            row_str = ["." for x in range(self.width)]
-            self.empty_canvas_matrix.append(row_str)
-            self.canvas_matrix.append(row_str)
+        self.clear_shapes()
 
     def print_canvas(self):
         for row in self.canvas_matrix:
@@ -16,27 +12,23 @@ class Canvas():
 
     def add_shape(self, added_shape):
         self.shapes.append(added_shape)
+        self.clear_shapes()
+        self.print_canvas()
+        canvas_matrix = self.canvas_matrix
         for shape in self.shapes:
             new_shape = shape.shape_matrix
-            canvas_matrix = self.empty_canvas_matrix
-            # canvas_matrix.reverse()
-            new_canvas_matrix = []
             for index, row in enumerate(canvas_matrix):
-                new_row = row
                 if index >= len(new_shape):
-                    new_canvas_matrix.append(new_row)
                     continue
                 shape_row = new_shape[-(index+1)]
                 shape_index = 0
-                while shape_index < len(shape_row):
+                while (shape_index < len(shape_row)) & (shape_index < len(row)):
                     if shape_row[shape_index] != ".":
-                        new_row[shape_index] = shape_row[shape_index]
+                        row[shape_index] = shape_row[shape_index]
                     shape_index += 1
-                new_canvas_matrix.append(new_row)
+        canvas_matrix.reverse()
 
-            new_canvas_matrix.reverse()
-
-        self.canvas_matrix = new_canvas_matrix
+        self.canvas_matrix = canvas_matrix
 
     def clear_shapes(self):
         self.canvas_matrix = []
@@ -109,9 +101,8 @@ def clear_shapes(canvas):
     canvas.clear_shapes()
 
 
-def create_rectange(canvas, start_x, start_y, end_x, end_y, fill_char):
+def create_rectangle(start_x, start_y, end_x, end_y, fill_char):
     new_rect = Shape(start_x, start_y, end_x, end_y, fill_char)
-    canvas.add_shape(new_rect)
     return new_rect
 
 
@@ -125,27 +116,3 @@ def translate(canvas, rect, axis, num):
     canvas.shapes.remove(rect)
     rect.translate(axis, num)
     canvas.add_shape(rect)
-
-    # c = Canvas(10, 10)
-    # c.print_canvas()
-    # print("-"*50)
-    # s = Shape(1, 4, 3, 2, 'a')
-    # s.print_shape()
-    # print("-"*50)
-    # s.change_fill_char('b')
-    # s.print_shape()
-    # print("-"*50)
-    # s.translate('y', 6)
-    # s.print_shape()
-    # print("-"*50)
-    # c.add_shape(s)
-    # c.print_canvas()
-    # print("-"*50)
-    # s2 = Shape(2, 5, 3, 2, 'a')
-    # s2.print_shape()
-    # print("-"*50)
-    # c.add_shape(s2)
-    # c.print_canvas()
-    # print("-"*50)
-    # c.clear_shapes()
-    # c.print_canvas()
