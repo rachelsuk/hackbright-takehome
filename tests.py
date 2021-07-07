9deade9757a1fad71dbac3bc6cccc50743e04fc7
Rachel <rachelsuk97@gmail.com>
2021-07-07 04:55:44 +0000

    added add_shape test

--- tests.py
+++ tests.py
@@ -1,12 +1,20 @@
 import unittest
 from takehome import Canvas, Shape
+import takehome
 
 
 class ShapeTestCase(unittest.TestCase):
     def test_create_shape(self):
         s = Shape(1, 1, 2, 0, "$")
-        test_shape_matrix = [[".", "$", "$"], [".", "$", "$"]]
+        test_shape_matrix = [[" ", "$", "$"], [" ", "$", "$"]]
         self.assertEqual(s.shape_matrix, test_shape_matrix)
+
+    def test_add_shape(self):
+        c = Canvas(2, 3)
+        s = Shape(1, 1, 2, 0, "$")
+        takehome.add_shape_to_canvas(c, s)
+        test_canvas_matrix = [[" ", "$", "$"], [" ", "$", "$"]]
+        self.assertEqual(c.canvas_matrix, test_canvas_matrix)
 
 
 if __name__ == "__main__":
